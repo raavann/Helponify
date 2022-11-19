a582a295c6e2855de4557c11aa9b9fee4f32d39c
Anurudh Mhatre <59391974+raavann@users.noreply.github.com>
2022-11-19 12:07:51 +0530

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,9 @@ $( function() {
     $( ".fab-wrapper" ).draggable({containment :"document"});
     
     // different sections
-    $("#chat").load("../../views/chat.html"); 
-    $("#knowMore").load("../../views/knowMore.html"); 
-    $("#about").load("../../views/about.html");
+    $("#chat").load("/views/chat.html"); 
+    $("#knowMore").load("/views/knowMore.html"); 
+    $("#about").load("/views/about.html");
 
 } );
 $(document).ready(function(){
